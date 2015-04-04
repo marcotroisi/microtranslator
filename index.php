745497ed91955769c6f23471c2f478e90af9e10e
Marco Troisi <hello@marcotroisi.com>
2015-04-04 14:23:38 +0200

    1st commit: Base Application

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author Marco Troisi
+ * @created 03.04.15
+ */
+
+require 'vendor/autoload.php';
+
+$f3 = Base::instance();
+
+/**
+ * Routes
+ */
+
+// Home
+$f3->route('GET /',
+    function() {
+        echo 'Hello, world!';
+    }
+);
+
+// Gets All Available Locales
+
+// Gets All Terms for a specific Locale
+
+// Counts All Terms for a specific Locale
+
+// Gets a Term for a specific Locale
+
+// Gets Untranslated Terms for a specific Locale
+
+// Counts Untranslated Terms for a specific Locale
+
+
+$f3->run();
