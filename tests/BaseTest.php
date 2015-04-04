922bc356e45fde5f58d3687e622c15ffde0ca4e6
Marco Troisi <inseguitoredidio@gmail.com>
2015-04-04 15:37:57 +0200

    Tests Added

---
+++ tests/BaseTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Marco Troisi
+ * @created 04.04.15
+ */
+
+namespace MicroTranslator\Test;
+
+
+class BaseTest extends \PHPUnit_Framework_TestCase {
+    public function testHello()
+    {
+        $var = "Hello World";
+
+        $this->assertEquals($var, "Hello World");
+    }
+}
